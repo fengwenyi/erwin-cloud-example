355d44f6b5a306e8a41e112ae25a98391ed644ed
冯文议 <xfsy_2015@163.com>
2021-02-07 15:41:11 +0800

    feat: spring cloud sleuth zipkin 失败

--- erwin-cloud-business/erwin-cloud-message/src/main/java/com/fengwenyi/example/erwincloudmessage/controller/UserController.java
+++ erwin-cloud-business/erwin-cloud-message/src/main/java/com/fengwenyi/example/erwincloudmessage/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.fengwenyi.example.erwincloudmessage.controller;
 
 import com.fengwenyi.api.result.ResultTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Erwin Feng
@@ -12,13 +15,15 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping("name")
-    public String name() {
+    public String name(HttpServletRequest request) {
+        log.info("message service, traceId={}, spanId={}", request.getHeader("X-B3-TraceId"), request.getHeader("X-B3-SpanId"));
         return restTemplate.getForEntity("http://erwin-cloud-user/user/getUserName", String.class).getBody();
     }
 
--- erwin-cloud-business/erwin-cloud-user/src/main/java/com/fengwenyi/example/erwinclouduser/controller/UserController.java
+++ erwin-cloud-business/erwin-cloud-user/src/main/java/com/fengwenyi/example/erwinclouduser/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.fengwenyi.example.erwinclouduser.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Erwin Feng
@@ -10,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @GetMapping("/getUserName")
-    public String getUserName() {
+    public String getUserName(HttpServletRequest request) {
+        log.info("user service, traceId={}, spanId={}", request.getHeader("X-B3-TraceId"), request.getHeader("X-B3-SpanId"));
         return "Zhangsan";
     }
 
